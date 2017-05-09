a263c1a4d4995707c2ff8e67952b3448dab71492
Thulio Philipe <thulioph@gmail.com>
2017-05-09 20:08:42 -0300

    First commit.

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+// React
+import React, { Component } from 'react';
+
+// Assets
+import logo from '../images/logo.svg';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo" />
+
+          <h2>Welcome to React</h2>
+        </div>
+
+        <p className="App-intro">
+          To get started, edit `src/App.js` and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import App from './components/App';
+
+// Assets
+import './css/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
