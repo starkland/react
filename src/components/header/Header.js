dcfcd078e91484c31d1dbabacced401c9afc12bb
Thulio Philipe <thulioph@gmail.com>
2017-05-09 20:49:06 -0300

    Creates the component using a function.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,46 +1,52 @@
 // React
-import React, { Component } from 'react';
+import React from 'react';
 
 // Assets
 const reactLogo = 'http://reactjs.cn/react/img/logo.svg';
 import '../../css/Header.css';
 
-class Header extends Component {
-	render() {
-		return(
-			<nav className="nav dark">
-				<div className="nav-left">
-					<a className="nav-item" target="_blank" href="http://reactjs.cn/">
-						<img src={reactLogo} alt="React logo"/>
-					</a>
-				</div>
+function _someThing() {
+	// should change the element class
+	// nav-toggle & nav-menu
 
-				<div className="nav-center">
-					<a className="nav-item" href="https://github.com/starkland/react" target="_blank">
-						<span className="icon">
-							<i className="fa fa-github"></i>
-						</span>
-					</a>
+	console.warn('123');
+}
 
-					<a className="nav-item" href="https://twitter.com/thulioph_" target="_blank">
-						<span className="icon">
-							<i className="fa fa-twitter"></i>
-						</span>
-					</a>
-				</div>
+function Header() {
+	return (
+		<nav className="nav dark">
+			<div className="nav-left">
+				<a className="nav-item" target="_blank" href="http://reactjs.cn/">
+					<img src={reactLogo} alt="React logo"/>
+				</a>
+			</div>
 
-				<span className="nav-toggle">
-					<span></span>
-					<span></span>
-					<span></span>
-				</span>
+			<div className="nav-center">
+				<a className="nav-item" href="https://github.com/starkland/react" target="_blank">
+					<span className="icon">
+						<i className="fa fa-github"></i>
+					</span>
+				</a>
 
-				<div className="nav-right nav-menu">
-					<a className="nav-item">Home</a>
-				</div>
-			</nav>
-		);
-	}
+				<a className="nav-item" href="https://twitter.com/thulioph_" target="_blank">
+					<span className="icon">
+						<i className="fa fa-twitter"></i>
+					</span>
+				</a>
+			</div>
+
+			<span className="nav-toggle"
+				onClick={_someThing}>
+				<span></span>
+				<span></span>
+				<span></span>
+			</span>
+
+			<div className="nav-right nav-menu">
+				<a className="nav-item">Home</a>
+			</div>
+		</nav>
+	);
 }
 
 export default Header;
