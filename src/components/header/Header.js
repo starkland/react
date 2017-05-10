3a5e175dcae4f55ef3248e40bab9fe20796bf662
Thulio Philipe <thulioph@gmail.com>
2017-05-10 14:39:46 -0300

    Toggle the navbar menu on click.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,48 +5,58 @@ import React from 'react';
 const reactLogo = 'http://reactjs.cn/react/img/logo.svg';
 import '../../css/Header.css';
 
-function _someThing() {
-	// should change the element class
-	// nav-toggle & nav-menu
 
-	console.warn('123');
-}
+class Header extends React.Component {
+	constructor(props) {
+		super(props);
 
-function Header() {
-	return (
-		<nav className="nav dark">
-			<div className="nav-left">
-				<a className="nav-item" target="_blank" href="http://reactjs.cn/">
-					<img src={reactLogo} alt="React logo"/>
-				</a>
-			</div>
+		this.state = {
+			isActive: false
+		};
+	}
 
-			<div className="nav-center">
-				<a className="nav-item" href="https://github.com/starkland/react" target="_blank">
-					<span className="icon">
-						<i className="fa fa-github"></i>
-					</span>
-				</a>
+	toggleClass() {
+		const currentState = this.state.isActive;
+		this.setState({ isActive: !currentState });
+	}
 
-				<a className="nav-item" href="https://twitter.com/thulioph_" target="_blank">
-					<span className="icon">
-						<i className="fa fa-twitter"></i>
-					</span>
-				</a>
-			</div>
+	render() {
+		return (
+			<nav className="nav dark">
+				<div className="nav-left">
+					<a className="nav-item" target="_blank" href="http://reactjs.cn/">
+						<img src={reactLogo} alt="React logo"/>
+					</a>
+				</div>
 
-			<span className="nav-toggle"
-				onClick={_someThing}>
-				<span></span>
-				<span></span>
-				<span></span>
-			</span>
+				<div className="nav-center">
+					<a className="nav-item" href="https://github.com/starkland/react" target="_blank">
+						<span className="icon">
+							<i className="fa fa-github"></i>
+						</span>
+					</a>
 
-			<div className="nav-right nav-menu">
-				<a className="nav-item">Home</a>
-			</div>
-		</nav>
-	);
+					<a className="nav-item" href="https://twitter.com/thulioph_" target="_blank">
+						<span className="icon">
+							<i className="fa fa-twitter"></i>
+						</span>
+					</a>
+				</div>
+
+				<span className={`nav-toggle ${this.state.isActive ? 'is-active': ''}`}
+					onClick={this.toggleClass.bind(this)}>
+					<span></span>
+					<span></span>
+					<span></span>
+				</span>
+
+				<div
+					className={`nav-right nav-menu ${this.state.isActive ? 'is-active': ''}`}>
+					<a className="nav-item">Home</a>
+				</div>
+			</nav>
+		);
+	}
 }
 
 export default Header;
