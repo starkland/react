ccfc38de2479c8d4f1f026b137a7b7177b2b86f4
Thulio Philipe <thulioph@gmail.com>
2017-05-10 15:56:40 -0300

    Adds the subheader component.

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,23 @@ import React, { Component } from 'react';
 
 // Components
 import Header from './header/Header';
+import Subheader from './subheader/Subheader';
 
 // Assets
 import '../css/App.css';
 
 class App extends Component {
   render() {
+  	let subtitle = `Search by user or repository.`;
+
     return (
-      <Header />
+    	<div>
+	      <Header />
+
+	      <Subheader
+	      	title="Home"
+	      	subtitle={subtitle} />
+    	</div>
     );
   }
 }
