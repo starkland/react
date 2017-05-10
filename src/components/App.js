69fd84a67047686e44722227a44591bd278b6268
Thulio Philipe <thulioph@gmail.com>
2017-05-10 18:30:13 -0300

    Adds the alert component.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import Header from './header/Header';
 import Subheader from './subheader/Subheader';
 import Form from './form/Form';
+import Alert from './alert/Alert';
 
 // Assets
 import '../css/App.css';
@@ -12,16 +13,17 @@ import '../css/App.css';
 class App extends Component {
   render() {
   	let subtitle = `Search by user or repository.`;
+  	let message = `O campo search deve ser preenchido.`;
 
     return (
     	<div>
 	      <Header />
 
-	      <Subheader
-	      	title="Home"
-	      	subtitle={subtitle} />
+	      <Subheader title="Home" subtitle={subtitle} />
 
-	      	<Form />
+      	<Form />
+
+      	<Alert message={message} />
     	</div>
     );
   }
