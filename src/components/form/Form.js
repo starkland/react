7751f072d7c1e363e13edbf2eecb561cff1213a3
Thulio Philipe <thulioph@gmail.com>
2017-05-11 14:21:06 -0300

    Use the alert component inside form.

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,13 +1,17 @@
 // React
 import React, { Component } from 'react';
 
+// Components
+import Alert from '../alert/Alert';
+
 class Form extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			input: '',
-			select: 'users'
+			select: 'users',
+			alertMsg: ''
 		};
 
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,47 +27,65 @@ class Form extends Component {
 	}
 
 	handleSubmit(evt) {
-		console.warn('Os dados foram:', this.state);
 		evt.preventDefault();
+
+		if (!this.state.input) {
+			this.setState({
+				alertMsg: 'Digite ao menos um termo para pesquisar.'
+			});
+		}
+
+		console.warn('Os dados foram:', this.state);
 	}
 
 	render() {
+		const hasError = this.state.alertMsg;
+		let alert = null;
+
+		if (hasError) {
+			alert = <Alert message={this.state.alertMsg} />;
+		}
+
 		return(
-			<form onSubmit={this.handleSubmit}>
-				<div className="field">
-				  <label className="label">Search by:</label>
-				  <p className="control">
-				    <span className="select">
-				      <select name="select" onChange={this.handleInputChange} value={this.state.select}>
-				        <option value="users">User</option>
-				        <option value="repositories">Repositories</option>
-				      </select>
-				    </span>
-				  </p>
-				</div>
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<div className="field">
+					  <label className="label">Search by:</label>
+					  <p className="control">
+					    <span className="select">
+					      <select name="select" onChange={this.handleInputChange} value={this.state.select}>
+					        <option value="users">User</option>
+					        <option value="repositories">Repositories</option>
+					      </select>
+					    </span>
+					  </p>
+					</div>
 
-				<div className="field">
-				  <label className="label"></label>
-				  <p className="control">
-				    <input
-				    	className="input"
-				    	type="text"
-				    	name="input"
-				    	value={this.state.input}
-				    	onChange={this.handleInputChange}
-				    	placeholder="Search here" />
-				  </p>
-				</div>
+					<div className="field">
+					  <label className="label"></label>
+					  <p className="control">
+					    <input
+					    	className="input"
+					    	type="text"
+					    	name="input"
+					    	value={this.state.input}
+					    	onChange={this.handleInputChange}
+					    	placeholder="Search here" />
+					  </p>
+					</div>
 
-				<div className="field is-grouped">
-				  <label className="label"></label>
-				  <p className="control">
-				    <button className="button is-primary">
-				      Search
-				    </button>
-				  </p>
-				</div>
-			</form>
+					<div className="field is-grouped">
+					  <label className="label"></label>
+					  <p className="control">
+					    <button className="button is-primary">
+					      Search
+					    </button>
+					  </p>
+					</div>
+				</form>
+
+				{alert}
+			</div>
 		);
 	}
 }
