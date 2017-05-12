729dcdcd11b165da1394bedb87989d8d14432f73
Thulio Philipe <thulioph@gmail.com>
2017-05-12 19:22:26 -0300

    Adjusts the notfound view.

--- src/views/notfound/NotFound.js
+++ src/views/notfound/NotFound.js
@@ -1,10 +1,32 @@
 // React
 import React from 'react';
 
+// Components
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+
 class NotFound extends React.Component {
 	render() {
 		return(
-			<h1>NotFound!</h1>
+			<div>
+				<Header />
+
+				<section className="hero is-large is-dark is-bold">
+				  <div className="hero-body">
+				    <div className="container">
+				      <h1 className="title">
+				        404 - Not found
+				      </h1>
+
+				      <h2 className="subtitle">
+				        You tried to access one page that not exists.
+				      </h2>
+				    </div>
+				  </div>
+				</section>
+
+				<Footer />
+			</div>
 		);
 	}
 }
