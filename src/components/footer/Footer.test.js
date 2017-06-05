db06d71fe07db5695f5d94616ba9cf822ff72425
Thulio Philipe <thulioph@gmail.com>
2017-06-05 19:52:41 -0300

    Add the footer test case.

--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -6,9 +6,11 @@ import Component from './Footer';
 // ====
 
 test('Testando o componente do Footer', () => {
+	const text = 'Made with  and  by Thulio Philipe';
+
 	const footer = shallow(
 		<Component />
 	);
 
-	expect(footer.text()).toBe('Made with  and  by Thulio Philipe');
+	expect(footer.text()).toBe(text);
 });
