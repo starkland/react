4d4cf9fcd9fabb8a5b7b6452ae81ed93d63eceaa
Thulio Philipe <thulioph@gmail.com>
2017-06-13 18:13:09 -0300

    Writes a new test case to header component.

--- src/components/table/Table.test.js
+++ src/components/table/Table.test.js
@@ -41,15 +41,45 @@ test('Deve ter 4 colunas na tabela de usuários.', () => {
 		<Component type={props.type} data={props.data} />
 	);
 
-	console.warn(table);
+	expect(table.find('thead th').length).toBe(4);
+});
+
+test('Deve ter os valores das props setados corretamente.', () => {
+	const props = {
+		type: 'users',
+		data: [{
+			"avatar_url" : "https://avatars3.githubusercontent.com/u/127790?v=3",
+			"events_url" : "https://api.github.com/users/dscho/events{/privacy}",
+			"followers_url" : "https://api.github.com/users/dscho/followers",
+			"following_url" : "https://api.github.com/users/dscho/following{/other_user}",
+			"gists_url" : "https://api.github.com/users/dscho/gists{/gist_id}",
+			"gravatar_id" : "",
+			"html_url" : "https://github.com/dscho",
+			"id" : 127790,
+			"login" : "dscho",
+			"organizations_url" : "https://api.github.com/users/dscho/orgs",
+			"received_events_url" : "https://api.github.com/users/dscho/received_events",
+			"repos_url" : "https://api.github.com/users/dscho/repos",
+			"score" : 15.500255,
+			"site_admin" : false,
+			"starred_url" : "https://api.github.com/users/dscho/starred{/owner}{/repo}",
+			"subscriptions_url" : "https://api.github.com/users/dscho/subscriptions",
+			"type" : "User",
+			"url" : "https://api.github.com/users/dscho"
+		}]
+	};
+
+	const table = mount(
+		<Component type={props.type} data={props.data} />
+	);
+
+	expect(table.getNode().props.type).toBe(props.type);
+	expect(table.getNode().props.data).toBe(props.data);
 
 	// const texts = table.find('thead th').map(node => node.text());
+	// console.warn();
 	// const array = ['Image', 'Username', 'Score', ''];
 
-	// expect(texts).toBe(array);
-	// expect(texts).to.have.length(4);
 
-	// console.warn(table.find('thead th').toHave.length(4));
 	// expect(texts.equals(array));
-
-})
+});
